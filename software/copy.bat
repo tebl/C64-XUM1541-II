e53010263b6ea9379eb5b47c023c3d7dd00f4b54
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2021-04-07 22:48:54 +0200

    Updated copy batch

--- software/copy.bat
+++ software/copy.bat
@@ -5,12 +5,15 @@ if not exist "disks" mkdir "disks"
 if "%~1"=="" (
     echo "Error: Please specify disk number!"
     echo copy.bat ^<disk number^>
+	pause
 ) else (
-    echo Copying disk %1 side A
-    d64copy.exe -n 8 "disks\D%1A.d64" > "disks\D%1A.log" 2>&1
+	echo Copying disk %1
+    echo   Copying disk %1 side A
     pause
-    echo Copying disk %1 side B
+    d64copy.exe -n 8 "disks\D%1A.d64" > "disks\D%1A.log" 2>&1
+    
+	echo   Copying disk %1 side B
+	pause
     d64copy.exe -n 8 "disks\D%1B.d64" > "disks\D%1B.log" 2>&1
     echo done!
 )
-pause
